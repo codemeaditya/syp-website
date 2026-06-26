8c35749bab5624285ffb3293c6f61f152e6db7ca
codemeaditya <codemeaditya123@gmail.com>
2026-06-26 11:17:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -220,3 +220,31 @@ window.addEventListener("load", () => {
     document.body.style.opacity = "1";
     
 });
+const counters=document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.getAttribute("data-target");
+const count=+counter.innerText;
+
+const increment=target/100;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target+"+";
+
+}
+
+}
+
+update();
+
+});
