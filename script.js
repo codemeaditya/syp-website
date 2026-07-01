9dd173c9a070c9461f0a59c784db445e54d93448
codemeaditya <codemeaditya123@gmail.com>
2026-07-01 09:02:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -266,3 +266,35 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(statsSection);
+
+/* =========================
+   BACK TO TOP BUTTON
+========================= */
+
+const backToTop =
+document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 400){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+        behavior:"smooth"
+
+    });
+
+});
